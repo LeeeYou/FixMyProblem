cfb0cf97f21ad0ad00155df9e4ddfca38854c85a
LeeeYou <leeeyou@163.com>
2017-03-18 14:38:09 +0800

    update MainActivity

--- app/src/main/java/com/example/leeeyou/fixmyproblem/view/activity/MainActivity.java
+++ app/src/main/java/com/example/leeeyou/fixmyproblem/view/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.leeeyou.fixmyproblem.view.activity;
 
-import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
 
 import com.example.leeeyou.fixmyproblem.R;
 import com.example.leeeyou.fixmyproblem.model.vo.IndexView;
@@ -11,7 +14,20 @@ import com.example.leeeyou.fixmyproblem.util.ViewHolder;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends ListActivity {
+import butterknife.BindArray;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.lv_index)
+    ListView lv_index;
+
+    @BindArray(R.array.index_title)
+    String[] index_title;
+
+    @BindArray(R.array.index_title_desc)
+    String[] index_title_desc;
 
     UniversalAdapter<IndexView> adapter;
     List<IndexView> dataList = new ArrayList<>();
@@ -20,117 +36,73 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        String[] index_title = getResources().getStringArray(R.array.index_title);
-        String[] index_title_desc = getResources().getStringArray(R.array.index_title_desc);
+        initData();
+        initAdapter();
+    }
 
+    private void initAdapter() {
+        if (adapter == null) {
+            adapter = new UniversalAdapter<IndexView>(this, dataList, R.layout.item_index_activity) {
+                @Override
+                public void convert(ViewHolder vh, IndexView item, final int position) {
+                    vh.setText(R.id.tv_title, dataList.get(position).getTitle());
+                    vh.setText(R.id.tv_title_desc, dataList.get(position).getDesc());
 
+                    vh.setOnClickListener(R.id.ll_item, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            switch (position) {
+                                case 0:
+                                    startActivity(new Intent(MainActivity.this, Problem01_ImageActivity.class));
+                                    break;
+                                case 1:
+                                    startActivity(new Intent(MainActivity.this, Problem02_GlideActivity.class));
+                                    break;
+                                case 2:
+                                    startActivity(new Intent(MainActivity.this, Problem03_SlidingConflictActivity.class));
+                                    break;
+                                case 3:
+                                    startActivity(new Intent(MainActivity.this, Problem04_ShapeSourceActivity.class));
+                                    break;
+                                case 4:
+                                    startActivity(new Intent(MainActivity.this, Problem05_EventBusActivity.class));
+                                    break;
+                                case 5:
+                                    startActivity(new Intent(MainActivity.this, Problem06_EventDispatchActivity.class));
+                                    break;
+                                case 6:
+                                    startActivity(new Intent(MainActivity.this, Problem07_Fragment_LifeCycle_Activity.class));
+                                    break;
+                                case 7:
+                                    startActivity(new Intent(MainActivity.this, Problem08_Math_Activity.class));
+                                    break;
+                                case 8:
+                                    startActivity(new Intent(MainActivity.this, Problem09_ViewDragHelper_Activity.class));
+                                    break;
+                                case 9:
+                                    startActivity(new Intent(MainActivity.this, Problem10_JSONArray_Activity.class));
+                                    break;
+                                case 10:
+                                    startActivity(new Intent(MainActivity.this, Problem11_CoordinatorLayout_Activity.class));
+                                    break;
+                            }
+                        }
+                    });
+                }
+            };
+        }
+        lv_index.setAdapter(adapter);
+    }
+
+    private void initData() {
         for (int i = 0; i < index_title.length; i++) {
             IndexView data = new IndexView();
             data.setTitle(index_title[i]);
             data.setDesc(index_title_desc[i]);
             dataList.add(data);
         }
-
-//        Button btn_problem_01 = (Button) findViewById(R.id.btn_problem_01);
-//        btn_problem_01.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem01_ImageActivity.class));
-//            }
-//        });
-//
-//        Button btn_problem_02 = (Button) findViewById(R.id.btn_problem_02);
-//        btn_problem_02.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem02_GlideActivity.class));
-//            }
-//        });
-//
-//        Button btn_problem_03 = (Button) findViewById(R.id.btn_problem_03);
-//        btn_problem_03.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem03_SlidingConflictActivity.class));
-//            }
-//        });
-//
-//        Button btn_problem_04 = (Button) findViewById(R.id.btn_problem_04);
-//        btn_problem_04.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem04_ShapeSourceActivity.class));
-//            }
-//        });
-//
-//        Button btn_problem_05 = (Button) findViewById(R.id.btn_problem_05);
-//        btn_problem_05.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem05_EventBusActivity.class));
-//            }
-//        });
-//
-//        Button btn_problem_06 = (Button) findViewById(R.id.btn_problem_06);
-//        btn_problem_06.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem06_EventDispatchActivity.class));
-//            }
-//        });
-//
-//        Button btn_problem_07 = (Button) findViewById(R.id.btn_problem_07);
-//        btn_problem_07.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem07_Fragment_LifeCycle_Activity.class));
-//            }
-//        });
-//
-//        Button btn_problem_08 = (Button) findViewById(R.id.btn_problem_08);
-//        btn_problem_08.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem08_Math_Activity.class));
-//            }
-//        });
-//
-//        Button btn_problem_09 = (Button) findViewById(R.id.btn_problem_09);
-//        btn_problem_09.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem09_ViewDragHelper_Activity.class));
-//            }
-//        });
-//
-//        Button btn_problem_10 = (Button) findViewById(R.id.btn_problem_10);
-//        btn_problem_10.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem10_JSONArray_Activity.class));
-//            }
-//        });
-//
-//        Button btn_problem_11 = (Button) findViewById(R.id.btn_problem_11);
-//        btn_problem_11.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, Problem11_CoordinatorLayout_Activity.class));
-//            }
-//        });
-
-        if (adapter == null) {
-            adapter = new UniversalAdapter<IndexView>(this, dataList, R.layout.item_index_activity) {
-                @Override
-                public void convert(ViewHolder vh, IndexView item, int position) {
-                    vh.setText()
-                }
-            };
-        }
-
-        setListAdapter(adapter);
-
     }
 
 }
